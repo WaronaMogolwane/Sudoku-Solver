fbd2ed5e6dfe58b1c52b42a1f9d3e8dc92c200e5
waronamogolwane <mogolwanew@gmail.com>
2022-02-10 20:33:56 +0200

    All functionality for the Sudoku Solver implemented

--- src/com/majorxp/SudokuSolver.java
+++ src/com/majorxp/SudokuSolver.java
@@ -2,6 +2,8 @@ package com.majorxp;
 
 public class SudokuSolver {
 
+    public static final int GRID_SIZE = 9;
+
     public static int [][] grid = {
 
             {3, 0, 6, 5, 0, 8, 4, 0, 0},
@@ -17,11 +19,19 @@ public class SudokuSolver {
 
     public static void main(String[] args) {
 
-        printUnsolvedGrid();
+        System.out.println("Unsolved Puzzle");
+        printGrid(grid);
+
+        if(solvePuzzle(grid)){
+            System.out.println("Puzzle successfully solved!");
+        }
+        else {
+            System.out.println("Solving the puzzle was unsuccessful.");
+        }
 
     }
 
-    private static void printUnsolvedGrid() {
+    private static void printGrid(int grid[][]) {
 
         System.out.println("--------------------------");
 
@@ -49,4 +59,70 @@ public class SudokuSolver {
 
 
     }
+
+    private static boolean solvePuzzle(int[][] grid){
+        for(int row = 0; row < GRID_SIZE; row++){
+            for(int column = 0; column < GRID_SIZE; column++){
+                if(grid[row][column] == 0){
+                    for(int numToTry = 1; numToTry <= GRID_SIZE; numToTry++){
+                        if(isPlacementValid(grid, numToTry, row, column)){
+                            grid[row][column] = numToTry;
+
+                            if(solvePuzzle(grid)){
+                                return true;
+                            }
+                            else {grid[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+
+        System.out.println("\n\n Solved Puzzle");
+        printGrid(grid);
+        return true;
+    }
+
+    private static boolean isPlacementValid(int[][] grid, int num, int row, int column){
+        return !isNumInRow(grid, num, row) &&
+                !isNumInColumn(grid, num, column) &&
+                !isNumInBox(grid, num, row, column);
+    }
+
+    private static boolean isNumInRow(int[][] grid, int num, int row){
+        for (int column = 0; column < GRID_SIZE; column++){
+            if(grid[row][column] == num){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumInColumn(int[][] grid, int num, int column){
+        for (int row = 0; row < GRID_SIZE; row++){
+            if(grid[row][column] == num){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isNumInBox(int[][] grid, int num, int row, int column){
+       int localBoxRow = row - (row % 3);
+       int localBoxColumn = column - (column % 3);
+
+       for(int i = localBoxRow; i < localBoxRow + 3; i++){
+           for(int j = localBoxColumn; j < localBoxColumn + 3; j++){
+               if(grid[i][j] == num){
+                   return true;
+               }
+           }
+       }
+       return false;
+    }
+
+
+
 }
